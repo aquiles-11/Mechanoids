444fd6c777c104acddc1a53e40c5d37d5fee3b81
Airo <59371341+Airomeda@users.noreply.github.com>
2025-08-08 02:18:24 +0800

    custom allegiance overlay rendering.

---
+++ 1.6/Source/PawnRenderNodeProperties_MechanoidAllegiance.cs
@@ -0,0 +1,15 @@
+﻿using Verse;
+
+namespace ApexMech
+{
+    public class PawnRenderNodeProperties_MechanoidAllegiance : PawnRenderNodeProperties
+    {
+        [NoTranslate]
+        public string maskPath;
+
+        public PawnRenderNodeProperties_MechanoidAllegiance()
+        {
+            nodeClass = typeof(PawnRenderNode_MechanoidAllegiance);
+        }
+    }
+}
