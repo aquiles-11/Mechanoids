e04c0ea57db4b4daf47dd5fc20de68874326c6ba
SaltyKarl <84060664+SaltyKarl@users.noreply.github.com>
2025-08-07 17:19:16 +0800

    1st pass

---
+++ 1.6/Source/Apex_render.cs
@@ -0,0 +1,47 @@
+﻿using RimWorld;
+using System;
+using UnityEngine;
+using Verse;
+
+namespace Apex.render
+{
+    public class PawnRenderNode_Cape : PawnRenderNode
+    {
+        public PawnRenderNode_Cape(Pawn pawn, PawnRenderNodeProperties props, PawnRenderTree tree)
+             : base(pawn, props, tree)
+        {
+        }
+
+        public override GraphicMeshSet MeshSetFor(Pawn pawn)
+        {
+            Pawn_StyleTracker style = pawn.style;
+            if (((style != null) ? style.beardDef : null) == null || pawn.style.beardDef.noGraphic)
+            {
+                return null;
+            }
+            return HumanlikeMeshPoolUtility.GetHumanlikeBeardSetForPawn(pawn, 1f, 1f);
+        }
+    }
+    public class PawnRenderNodeWorker_Cape : PawnRenderNodeWorker_FlipWhenCrawling
+    {
+        public override bool CanDrawNow(PawnRenderNode node, PawnDrawParms parms)
+        {
+            return base.CanDrawNow(node, parms) && (parms.facing != Rot4.North || parms.flipHead);
+        }
+
+        public override Vector3 OffsetFor(PawnRenderNode node, PawnDrawParms parms, out Vector3 pivot)
+        {
+            HeadTypeDef headType = parms.pawn.story.headType;
+            Vector3 vector = base.OffsetFor(node, parms, out pivot);
+            if (parms.facing == Rot4.East)
+            {
+                vector += Vector3.right * headType.beardOffsetXEast;
+            }
+            else if (parms.facing == Rot4.West)
+            {
+                vector += Vector3.left * headType.beardOffsetXEast;
+            }
+            return vector + (headType.beardOffset + parms.pawn.style.beardDef.GetOffset(headType, parms.facing));
+        }
+    }
+}
