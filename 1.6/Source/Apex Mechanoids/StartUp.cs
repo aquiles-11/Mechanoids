c10ab5bced103f801397bbc1d7394fc8138af44b
MaxDorob <maxdor81@gmail.com>
2025-09-26 22:33:06 +0600

    Harmony from nuget. Invoke all harmony patches

---
+++ 1.6/Source/Apex Mechanoids/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApexMechanoids
+{
+    [Verse.StaticConstructorOnStartup]
+    internal static class StartUp
+    {
+        static HarmonyLib.Harmony harmony;
+        static StartUp()
+        {
+            harmony = new HarmonyLib.Harmony("ApexMechanoids");
+            harmony.PatchAll();
+        }
+    }
+}
