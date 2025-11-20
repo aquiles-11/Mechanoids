868d707b8bed4f80cf4c12e314e8da64b466eef2
Shashlichnik <dmv_01@mail.ru>
2025-11-21 04:55:05 +0600

    Damage things on the way

---
+++ 1.6/Source/ApexMechanoids/SeveringEdge.cs
@@ -0,0 +1,56 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+
+namespace ApexMechanoids
+{
+    public class SeveringEdge : Projectile
+    {
+        const float maxDist = 1.9f;
+        private List<Thing> ignore = new List<Thing>();
+        protected override void Tick()
+        {
+            base.Tick();
+            foreach (var thing in Map.mapPawns.AllPawnsSpawned.Where(IsValidTarget).ToList())
+            {
+                Impact(thing);
+            }
+        }
+        protected virtual void Impact(Thing thing)
+        {
+            BattleLogEntry_RangedImpact battleLogEntry_RangedImpact = new BattleLogEntry_RangedImpact(this.launcher, thing, null, this.launcher.def, this.def, this.targetCoverDef);
+            Find.BattleLog.Add(battleLogEntry_RangedImpact);
+            DamageInfo dinfo = new DamageInfo(this.def.projectile.damageDef, (float)this.DamageAmount, this.ArmorPenetration, this.ExactRotation.eulerAngles.y, this.launcher, null, this.equipmentDef, DamageInfo.SourceCategory.ThingOrUnknown, this.intendedTarget.Thing, true, true, QualityCategory.Normal, true, false);
+            thing.TakeDamage(dinfo).AssociateWithLog(battleLogEntry_RangedImpact);
+
+            ignore.Add(thing);
+        }
+        protected virtual bool IsValidTarget(Thing target)
+        {
+            if (!(target is Pawn pawn))
+            {
+                return false;
+            }
+            if (ignore.Contains(target))
+            {
+                return false;
+            }
+            if ((target.DrawPos - DrawPos).MagnitudeHorizontalSquared() > maxDist * maxDist)
+            {
+                return false;
+            }
+
+            return true;
+        }
+        public override void ExposeData()
+        {
+            base.ExposeData();
+            Scribe_Collections.Look(ref ignore, nameof(ignore), LookMode.Reference);
+        }
+    }
+}
