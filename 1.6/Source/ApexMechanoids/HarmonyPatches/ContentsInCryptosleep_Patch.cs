45144c8e0031f31dd44c0c2f22b1deefedcb413d
SaltyKarl <84060664+SaltyKarl@users.noreply.github.com>
2025-12-11 00:59:56 +0800

    cleanup#2

--- 1.6/Source/ApexMechanoids/HarmonyPatches/ContentsInCryptosleep_Patch.cs
+++ 1.6/Source/ApexMechanoids/HarmonyPatches/ContentsInCryptosleep_Patch.cs
@@ -38,8 +38,6 @@ namespace ApexMechanoids
                     yield return new CodeInstruction(OpCodes.Brfalse_S, label);
                     yield return new CodeInstruction(OpCodes.Ldc_I4_1);
                     yield return new CodeInstruction(OpCodes.Ret);
-
-
                     yield return new CodeInstruction(OpCodes.Ldarg_0);
                     yield return instruction.WithLabels(label);
                     patched = true;
