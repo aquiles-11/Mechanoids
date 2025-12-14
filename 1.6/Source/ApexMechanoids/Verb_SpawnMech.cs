a700cb35aeefeb6723b9cb0e8e68e5a3a190735f
aranmaso <lalachaniscute@gmail.com>
2025-12-14 21:28:45 +0700

    smoke screen effect update

---
+++ 1.6/Source/ApexMechanoids/Verb_SpawnMech.cs
@@ -0,0 +1,85 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace ApexMechanoids
+{
+    public class Verb_SpawnMech : Verb_Shoot
+    {
+        public DefModExtension_MechPack modExtension => EquipmentSource.def.GetModExtension<DefModExtension_MechPack>();
+
+        public CompApparelReloadable comp
+        {
+            get
+            {
+                return EquipmentSource.TryGetComp<CompApparelReloadable>();
+            }
+        }
+
+        public List<Pawn> spawnedThing = new List<Pawn>();
+        public override void ExposeData()
+        {
+            base.ExposeData();
+            Scribe_Collections.Look(ref spawnedThing, "spawnedThing",LookMode.Reference);
+        }
+        public override bool TryCastShot()
+        {
+            if (comp != null)
+            {
+                try
+                {
+                    if (comp.remainingCharges > 0)
+                    {
+                        comp.UsedOnce();
+                        IReadOnlyList<Pawn> list = spawnedThing.ToList();
+                        foreach (var item in list)
+                        {
+                            if (item.Dead || item.DestroyedOrNull())
+                            {
+                                spawnedThing.Remove(item);
+                            }
+                        }
+                        if (spawnedThing.Count > 2)
+                        {                            
+                            Pawn pawn = spawnedThing.FirstOrDefault();
+                            pawn.Kill(new DamageInfo(DamageDefOf.ElectricalBurn,99999f,2f,instigator:Caster));
+                            spawnedThing.Remove(pawn);
+                        }
+                        Pawn spawnedOne = PawnGenerator.GeneratePawn(modExtension.spawnedKind);
+                        spawnedOne.SetFaction(Caster.Faction);
+                        GenSpawn.Spawn(spawnedOne, CurrentTarget.Cell, Caster.MapHeld);
+                        spawnedThing.Add(spawnedOne);
+                        return true;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Log.Error($"[ApexMechanoids] Error in Verb_SpawnMech.TryCastShot {ex}");
+                }
+            }
+            return false;
+        }
+
+        public override bool Available()
+        {
+            if (comp != null)
+            {
+                return comp.CanBeUsed(out var _);
+            }
+            return base.Available();
+        }
+    }
+
+    public class DefModExtension_MechPack : DefModExtension
+    {
+        public int maxNum = 2;
+
+        public PawnKindDef spawnedKind;
+
+        public float resourceConsumed = 1;
+    }
+}
