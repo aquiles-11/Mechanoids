2857953c406def4b475b523a23b4cc88ca3e1940
Shashlichnik <dmv_01@mail.ru>
2026-03-26 03:44:02 +0600

    Corrected animation start-end condition

---
+++ 1.6/Source/ApexMechanoids/JobDriver_Absorb.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse.AI;
+
+namespace ApexMechanoids
+{
+    public class JobDriver_Absorb : JobDriver_CastAbilityMelee
+    {
+        public override IEnumerable<Toil> MakeNewToils()
+        {
+            this.AddFinishAction((c) => pawn.drawer.renderer.SetAnimation(null));
+            foreach (var toil in base.MakeNewToils())
+            {
+                if (toil.debugName == "CastVerb")
+                {
+                    toil.initAction += () => pawn.drawer.renderer.SetAnimation(ApexDefsOf.APM_EatingIngestor);
+                }
+                yield return toil;
+            }
+        }
+    }
+}
